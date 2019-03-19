e24d1ab0a2b86d2d54a51967d251601870195e77
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-18 23:21:06 -0400

    Learning comments

---
+++ Comments.java
@@ -0,0 +1,12 @@
+    //  Demonstrating comments
+       public class Comments
+    {
+        public static void main(String[] args)
+        {
+             /*  This shows
+                 that these comments
+                 don't matter */
+            System.out.println("Hello"); //This line executes
+            // up to where the comment started
+        }
+    }

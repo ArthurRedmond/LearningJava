eb692253d1820fed3681191d50e301586ddee36f
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-19 00:12:16 -0400

    Another dialog box

---
+++ HelloDialog.java
@@ -0,0 +1,12 @@
+// Filename HelloDialog.java
+// Written by Arthur Redmond
+// Written on 2019-03-19
+
+import javax.swing.JOptionPane;
+public class HelloDialog
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+    }
+}

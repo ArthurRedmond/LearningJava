f410fc65e58c743faa60f6966da3b1c1d7ed8e67
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-18 23:53:23 -0400

    Dialog boxes

---
+++ Hello2.java
@@ -0,0 +1,12 @@
+// Filename Hello2.java
+// Written by Arthur Redmond
+// Written on 2019-03-18
+public class Hello2
+/* This class demonstrates the use of the println()
+   method to print the message Hello, world! */
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, world!");
+	}
+}

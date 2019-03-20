6641a7ee1fd3f5de4dc1d7baca796d550d514ff8
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-19 22:01:56 -0400

    Additional exercises

---
+++ CharDemo.java
@@ -0,0 +1,13 @@
+// Filename CharDemo.java
+// Written by Arthur Redmond
+// Written on 2019-03-19
+
+public class CharDemo
+{
+    public static void main(String[] args)
+    {
+        char initial = 'A';
+        System.out.println(initial);
+        System.out.println("\t\"abc\\def\bghi\n\njkl");
+    }
+}
---
+++ IntegerDemo.java
@@ -0,0 +1,19 @@
+// Filename IntegerDemo.java
+// Written by Arthur Redmond
+// Written on 2019-03-19
+
+public class IntegerDemo
+{
+    public static void main(String[] args)
+    {
+        int anInt = 12;
+        byte aByte = 12;
+        short aShort = 12;
+        long aLong = 12;
+
+        System.out.println("The int is   " + anInt);
+        System.out.println("The byte is  " + aByte);
+        System.out.println("The short is " + aShort);
+        System.out.println("The long is  " + aLong);
+    }
+}

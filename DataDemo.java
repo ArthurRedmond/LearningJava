ee23f1b5552aae83f767b066052ae7c75c2e7c10
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-19 21:28:54 -0400

    Create DataDemo

---
+++ DataDemo.java
@@ -0,0 +1,15 @@
+// Filename DataDemo.java
+// Written by Arthur Redmond
+// Written on 2019-03-19
+
+public class DataDemo
+{
+    public static void main(String[] args)
+    {
+        int aWholeNumber = 315;
+        final int STATES_IN_US = 50;
+        System.out.print("The number is ");
+        System.out.println(aWholeNumber);
+        System.out.println("The number of states is " + STATES_IN_US);
+    }
+}

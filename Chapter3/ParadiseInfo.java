8fc4364333e7aa8c557db2376f741f23218d3da2
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-21 21:32:38 -0400

    updated

--- Chapter3/ParadiseInfo.java
+++ Chapter3/ParadiseInfo.java
@@ -1,4 +1,16 @@
-// Filename ArithmeticDemo.java
+// Filename ParadiseInfo.java
 // Written by Arthur Redmond
-// Written on 2019-03-19
+// Written on 2019-03-21
 
+public class ParadiseInfo
+{
+    public static void main(String[] args)
+    {
+        displayInfo();
+    }
+    public static void displayInfo()
+    {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+}

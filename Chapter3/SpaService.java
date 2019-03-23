ddc57371788b2181f21542c6b18221449a2afca6
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-23 00:25:23 -0400

    updates

---
+++ Chapter3/SpaService.java
@@ -0,0 +1,26 @@
+// Filename SpaService.java
+// Written by Arthur Redmond
+// Written on 2019-03-21
+
+public class SpaService
+{
+    private String serviceDesctiption;
+    private double price;
+
+    public void setServiceDesctiption(String service)
+    {
+        serviceDesctiption = service;
+    }
+    public void setPrice(double servicePrice)
+    {
+        price = servicePrice;
+    }
+    public String getServiceDescription()
+    {
+        return serviceDesctiption;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+}

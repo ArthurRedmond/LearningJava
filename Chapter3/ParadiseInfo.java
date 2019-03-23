49b3b2820a9dcdc04d0c548d800b4897d051413f
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-22 23:58:34 -0400

    updates

--- Chapter3/ParadiseInfo.java
+++ Chapter3/ParadiseInfo.java
@@ -11,6 +11,6 @@ public class ParadiseInfo
     public static void displayInfo()
     {
         System.out.println("Paradise Day Spa wants to pamper you.");
-        System.out.println("We will make you look good.");
+        System.out.println("We will make you look good .");
     }
 }

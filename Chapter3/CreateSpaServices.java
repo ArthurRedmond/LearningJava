f7b83034a76f2074b48359ff8983bfc4efbda4b3
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-23 21:41:32 -0400

    Declaring objects

---
+++ Chapter3/CreateSpaServices.java
@@ -0,0 +1,13 @@
+// Filename CreateSpaServices.java
+// Written by Arthur Redmond
+// Written on 2019-03-23
+
+import java.util.Scanner;
+
+public class CreateSpaServices
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}

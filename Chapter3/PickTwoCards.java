14ea880c07020b658cb0bb3cd66c094bcc78f43c
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-25 12:35:37 -0400

    Updates to methods

--- Chapter3/PickTwoCards.java
+++ Chapter3/PickTwoCards.java
@@ -6,23 +6,29 @@ import java.util.Scanner;
 
 // To be used in calculations:
 // final int CARDS_IN_SUIT = 13;
-// myValue = ((int)(Math.random() * 100) % CARDS_IN_SUIT + );
+// RandomInt = ((int)(Math.random() * 100) % CARDS_IN_SUIT + );
 public class PickTwoCards
 {
     public static void main(String[] args)
     {
         Card firstCard = new Card();
+        firstCard.setCardSuit('s');
+        System.out.println("First card suit: " + firstCard.getCardSuit()); 
+        firstCard.setCardValue(getData());
+        System.out.println("First card value: " + firstCard.getCardValue());
         Card secondCard = new Card();
-        Scanner keyboard = new Scanner(System.in);
-        firstCard = getData(firstCard);
-        secondCard = getData(secondCard);
+        secondCard.setCardSuit('d');
+        System.out.println("\nSecond card suit: " + firstCard.getCardSuit()); 
+        secondCard.setCardValue(getData());
+        System.out.println("Second card value: " + firstCard.getCardValue());
+        //Scanner keyboard = new Scanner(System.in);
+        //firstCard = getData(firstCard);
+        //secondCard = getData(secondCard);
     }
-    public static Card getData(Card suit)
+    public static int getData()
     {
-        char suiT;
-        int cardValue;
-        Scanner keyboard = new Scanner(System.in);
-        
+        final int CARDS_IN_SUIT = 13;
+        RandomInt = ((int)(Math.random() * 100) % CARDS_IN_SUIT + 1);
+        return RandomInt
     }
-
 }

8ceb287fda7a0bd4df89b16a3d8e7e850ae39892
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-24 20:44:14 -0400

    GameZone Chapter 3 setup

---
+++ Chapter3/Card.java
@@ -0,0 +1,26 @@
+// Filename Card.java
+// Written by Arthur Redmond
+// Written on 2019-03-24
+
+public class Card
+{
+    private char suit;
+    private int cardValue;
+
+    public void setSuit(char suit)
+    {
+        // Code
+    }
+    public char getSuit()
+    {
+        return suit;
+    }
+    public void setCardValue()
+    {
+        // Code
+    }
+    public int getCardValue()
+    {
+        return cardValue;
+    }
+}

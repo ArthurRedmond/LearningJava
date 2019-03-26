f3827dce925845135c1e615787f2cfde13af37a2
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-26 18:34:09 -0400

    It Works!

--- Chapter3/PickTwoCards2.java
+++ Chapter3/PickTwoCards2.java
@@ -6,17 +6,17 @@ public class PickTwoCards2
     Card2 secondCard = new Card2();
     firstCard = getData(firstCard);
     secondCard = getData(secondCard);
-    System.out.println("Your first card is the " + getCardValue() + " of " + getCardSuit());
-    System.out.println("Your second card is the " + getCardValue() + " of " + getCardSuit());
+    System.out.println("Your first card is the " + firstCard.getCardSuit() + " of " + firstCard.getCardSuit());
+    System.out.println("Your second card is the " + secondCard.getCardValue() + " of " + secondCard.getCardSuit());
   }
   public static Card2 getData(Card2 data)
   {
     char suit;
     int myValue;
     data.setCardSuit('S');
-    data.setCardValue(myValue);
-    final int CARDS_IN_SUIT = 13;
+        final int CARDS_IN_SUIT = 13;
     myValue = ((int)(Math.random() * 100) % CARDS_IN_SUIT +1);
+    data.setCardValue(myValue);
     return data;
   }
 }

1140565e6961bf74f98dce7d223ace6b62e13bd8
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-26 12:20:39 -0400

    Create Card2.java

---
+++ Chapter3/Card2.java
@@ -0,0 +1,22 @@
+public class Card2
+{
+  private char cardSuit;
+  private int cardValue;
+  
+  public void setCardSuit(char suit)
+  {
+    cardSuit = suit;
+  }
+  public char getCardSuit()
+  {
+    return suit;
+  }
+  public void setCardValue(int myValue)
+  {
+    cardValue = myValue;
+  }
+  public int getCardValue()
+  {
+    return cardValue;
+  }
+}

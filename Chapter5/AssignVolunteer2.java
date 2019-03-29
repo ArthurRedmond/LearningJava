41178cbf006968d09c6bdbc3e752364ac70fce03
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-28 23:25:23 -0400

    Chapter5

--- Chapter5/AssignVolunteer2.java
+++ Chapter5/AssignVolunteer2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class AssignVolunteer
+public class AssignVolunteer2
 {
     public static void main(String[] args)
     {
@@ -9,6 +9,7 @@ public class AssignVolunteer
         final int OTHER_CODE = 2;
         final String CLOTHING_PRICER = "Regina";
         final String OTHER_PRICER = "Marco";
+        String message;
         Scanner input = new Scanner(System.in);
         System.out.println("What type of donation is this?");
         System.out.print("Enter " + CLOTHING_CODE + " for clothing, " + 
@@ -16,11 +17,18 @@ public class AssignVolunteer
         donationType = input.nextInt();
 
         if(donationType == CLOTHING_CODE)
+        {
           volunteer = CLOTHING_PRICER;
+          message = "a clothing donation";
+        }
         else
+        {
           volunteer = OTHER_PRICER;
+          message = "a non-clothing donation";
+        }
 
           System.out.println("You entered " + donationType);
+          System.out.println("This is " + message);
           System.out.println("The volunteer who will price this item is " + volunteer);
     }
 }

86380a056715b87813840b3301190e36957c1d51
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-03-31 00:23:29 -0400

    updated

--- Chapter5/AssignVolunteer4.java
+++ Chapter5/AssignVolunteer4.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import javax.swing.DebugGraphics;
 public class AssignVolunteer4
 {
     public static void main(String[] args)
@@ -6,34 +8,51 @@ public class AssignVolunteer4
         int donationType;
         String volunteer;
         final int CLOTHING_CODE = 1;
-        final int OTHER_CODE = 2;
+        final int FURNITURE_CODE = 2;
+        final int ELECTRONICS_CODE = 3;
+        final int OTHER_CODE = 4;
         final String CLOTHING_PRICER = "Regina";
         final String OTHER_PRICER = "Marco";
-        final String INVALID_PRICER = "invalid";
+        final String FURNITURE_PRICER = "Walter";
+        final String ELECTRONICS_PRICER = "Lydia";
         String message;
         Scanner input = new Scanner(System.in);
         System.out.println("What type of donation is this?");
-        System.out.print("Enter " + CLOTHING_CODE + " for clothing, " + 
-          OTHER_CODE + " for anything else... ");
+        System.out.print("Enter an integer... ");
         donationType = input.nextInt();
 
-        if(donationType <= OTHER_CODE)
-          if(donationType == CLOTHING_CODE)
+        switch(donationType)
+        {
+          case(CLOTHING_CODE):
           {
             volunteer = CLOTHING_PRICER;
             message = "a clothing donation";
+            break;
           }
-          else
+          case(FURNITURE_CODE):
+          {
+            volunteer = FURNITURE_PRICER;
+            message = "a furniture donation";
+            break;
+          }
+          case(ELECTRONICS_CODE):
+          {
+            volunteer = ELECTRONICS_PRICER;
+            message = "an electronics donation";
+            break;
+          }
+          case(OTHER_CODE):
           {
             volunteer = OTHER_PRICER;
-            message = "a non-clothing donation";
+            message = "another donation type";
+            break;
           }
-        else
-        {
-          volunteer = INVALID_PRICER;
-          message = "an invalid donation type";
+          default:
+          {
+            volunteer = "invalid";
+            message = "an invalid donation";
+          }
         }
-
           System.out.println("You entered " + donationType);
           System.out.println("This is " + message);
           System.out.println("The volunteer who will price this item is " + volunteer);

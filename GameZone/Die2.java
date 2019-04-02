fce191b84f636a21ee12e739ab7b6e8e309249b5
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-04-02 00:11:20 -0400

    It works!

--- GameZone/Die2.java
+++ GameZone/Die2.java
@@ -7,9 +7,9 @@ public class Die2
   public int dieValue;
   private final int HIGHEST_DIE_VALUE = 6;
   private final int LOWEST_DIE_VALUE = 1;
-
+ 
   public Die2()
   {
     dieValue = (((int)(Math.random() * 100) % HIGHEST_DIE_VALUE + LOWEST_DIE_VALUE));   
-  }    
-}
+  }
+ }
--- GameZone/TwoDice2.java
+++ GameZone/TwoDice2.java
@@ -10,34 +10,15 @@ public class TwoDice2
         Die2 secondDie = new Die2();
         int dieOne = firstDie.dieValue;
         int dieTwo = secondDie.dieValue;
-        int winningDie;
-        String message;
-
-
-
-
+        
         System.out.println("Throwing dice...");
-        if(dieOne > dieTwo)
-        {
-          winningDie = 1;
-            if(firstDie.dieValue < secondDie.dieValue)
-                winningDie = 2;
-        }
-          else
-          winningDie = 3;
         System.out.println("Die number one: " + firstDie.dieValue);
         System.out.println("Die number two: " + secondDie.dieValue);
-        switch(winningDie)
-        {
-            case 1:
+        if(dieOne > dieTwo)
             System.out.println("Die number one is higher!");
-            break;
-            case 2:
+        if(dieOne < dieTwo)
             System.out.println("Die number two is higher!");
-            break;
-            case 3:
+        if(dieOne == dieTwo)
             System.out.println("It's a tie!");
-            break;
-        }
     }
 }

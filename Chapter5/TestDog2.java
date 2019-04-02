3e344e3da3c3b0ee0ec5c6722a62764228b649ff
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-04-01 21:39:36 -0400

    upd

--- Chapter5/TestDog2.java
+++ Chapter5/TestDog2.java
@@ -4,9 +4,9 @@ public class TestDog2
     {
         DogTriathlonParticipant2 dog1 = new DogTriathlonParticipant2("Bowser", 2, 85, 89, 0);
         dog1.display();
-        DogTriathlonParticipant2 dog2 = new DogTriathlonParticipant2("Rush", 3, 78, 72, 80);
+        DogTriathlonParticipant2 dog2 = new DogTriathlonParticipant2("Rush", 2, 78, 72, 80);
         dog2.display();
-        DogTriathlonParticipant2 dog3 = new DogTriathlonParticipant2("Ginger", 3, 90, 86, 72);
+        DogTriathlonParticipant2 dog3 = new DogTriathlonParticipant2("Ginger", 3, 90, 86, 0);
         dog3.display();
     }
 }

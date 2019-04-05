66e0516efe2128456c2c86b2b7d8b3efb9bec099
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-04-04 22:21:00 -0400

    Counting game now works!

--- GameZone/Count21.java
+++ GameZone/Count21.java
@@ -8,30 +8,49 @@ public class Count21
     public static void main(String[] args)
     {
         String message = "Enter a number >> ";
-        String message2 = "The tally is ";
+        String message2 = "The tally is now ";
+        boolean gameOver = false;
         int playerNum;
         int computerNum;
         int tally = 0;
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Let's play a game. \nLet's take turn picking numbers between 1 and 3. " +
+        System.out.println("Let's play a game. \nLet's take turns picking numbers between 1 and 3. " +
         "\nThe first to go over 21 loses. \nYou can go first.");
-        do
+        System.out.println();
+        while(!gameOver)
         {
-            System.out.print(message);
+            System.out.print(message);      
             playerNum = keyboard.nextInt();
-            tally += playerNum;
-            System.out.println(message2 + tally + ".");
-            computerNum = RandomNum();
-            System.out.println("I'll add " + computerNum + ".");
-            tally += computerNum;
-            System.out.println(message2 + tally + ".");
-            System.out.println();
-
-        } while (tally < 21);
-        
-
+            if(playerNum < 1 || playerNum > 3)
+            {
+                System.out.println("Please only enter a number between 1 and 3.");
+            }
+            else
+            {
+                tally += playerNum;
+                System.out.println(message2 + tally + ".");   
+                System.out.println();
+                if(tally > 21)
+                {
+                    gameOver = true;
+                    System.out.println("Hah! You went over 21. You lose!");
+                }
+                else
+                {
+                    computerNum = RandomNum();
+                    System.out.println("I'll add " + computerNum + ".");
+                    tally += computerNum;
+                    System.out.println(message2 + tally + ".");
+                    System.out.println();
+                    if(tally > 21)
+                    {
+                        gameOver = true;
+                        System.out.println("Oh no! I went over 21. You win!");     
+                    }
+                }
+            }
+        }
     }
-
     public static int RandomNum()
     { 
         final int LOW_NUMBER = 1;
@@ -40,6 +59,4 @@ public class Count21
         randomNumber = (((int)(Math.random() * 100) % HIGH_NUMBER + LOW_NUMBER));
         return randomNumber;
     }
-
-
 }

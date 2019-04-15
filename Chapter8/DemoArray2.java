3b7f115a379fa1f251f72dfa0acac5efa228d172
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-04-15 12:37:48 -0400

    Update DemoArray2.java

--- Chapter8/DemoArray2.java
+++ Chapter8/DemoArray2.java
@@ -1,1 +1,11 @@
-
+public class DemoArray2{
+  public static void main(String[] args){
+    double[] salaries = {12.25, 13.55, 14.25, 16.85};
+    
+    System.out.println("Salaries one by one are:");
+    System.out.println(salaries[0]);
+    System.out.println(salaries[1]);
+    System.out.println(salaries[2]);
+    System.out.println(salaries[3]);
+  }
+}

360556b7cf57b48430087158485251adabaa0ea0
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-04-15 12:29:26 -0400

    Create DemoArray.java

---
+++ DemoArray.java
@@ -0,0 +1,17 @@
+public class DemoArray
+{
+  publc static void main(String[] args)
+  {
+    double[] salaries = new double[4];
+    salaries[0] = 12.25;
+    salaries[1] = 13.55;
+    salaries[2] = 14.25;
+    salaries[3] = 16.85;
+    
+    System.out.println("Salaries one by one are:");
+    System.out.println(salaries[0]);
+    System.out.println(salaries[1]);
+    System.out.println(salaries[2]);
+    System.out.println(salaries[3]);
+  }
+}

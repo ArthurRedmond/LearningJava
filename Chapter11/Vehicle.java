845f6de9ef86c85cbffe0210ea6ef039786fd905
ArthurRedmond <19720427+ArthurRedmond@users.noreply.github.com>
2019-04-23 23:15:30 -0400

    Chapter 11

---
+++ Chapter11/Vehicle.java
@@ -0,0 +1,34 @@
+public abstract class Vehicle
+{
+  private String powerSource;
+  private int wheels;
+  protected int price;
+
+  public Vehicle(String powerSource, int wheels)
+  {
+    setPowerSource(powerSource);
+    setWheels(wheels);
+    setPrice();
+  }
+  public String getPowerSource()
+  {
+    return powerSource;
+  }
+  public int getWheels()
+  {
+    return wheels;
+  }
+  public int getPrice()
+  {
+    return price;
+  }
+  public void setPowerSource(String source)
+  {
+    powerSource = source;
+  }
+  public void setWheels(int numWheels)
+  {
+    wheels = numWheels;
+  }
+  public abstract void setPrice();
+}
